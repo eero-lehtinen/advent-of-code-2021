21214a76f9dd1f6ce88ecf4b91baa8201b166c1b
Eero Lehtinen <eero@eerolehtinen.fi>
2022-06-25 00:30:47 +0300

    d6

---
+++ day06/app/part1.hs
@@ -0,0 +1,28 @@
+import Data.Either (rights)
+import Data.Sequence (Seq, (|>))
+import qualified Data.Sequence as Seq
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.Text.Read (decimal)
+
+type FishCount = Seq Int
+
+textLineToNumList :: T.Text -> T.Text -> [Int]
+textLineToNumList text sep = map fst $ rights $ map decimal $ T.splitOn sep text
+
+countOccur :: Eq a => a -> [a] -> Int
+countOccur x = length . filter (x ==)
+
+simulateFishDay :: FishCount -> FishCount
+simulateFishDay count =
+  let zeroCount = Seq.index count 0
+   in Seq.adjust (+ zeroCount) 6 $ Seq.drop 1 count |> zeroCount
+
+main :: IO ()
+main = do
+  contents <- TIO.readFile "input.txt"
+  let nums = textLineToNumList contents (T.pack ",")
+      count = fmap (\n -> countOccur n nums) (Seq.fromList [0 .. 8])
+
+  putStrLn . show . sum $ iterate simulateFishDay count !! 80
