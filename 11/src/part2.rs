253fb0932a4375ef0a5b30800368b4d776e44851
Eero Lehtinen <eero@eerolehtinen.fi>
2022-07-20 20:54:38 +0300

    11

---
+++ 11/src/part2.rs
@@ -0,0 +1,68 @@
+use std::fs;
+
+fn parse_num_line(line: &str) -> Vec<i32> {
+	line.chars()
+		.flat_map(|c| c.to_digit(10))
+		.map(|u| u as i32)
+		.collect()
+}
+
+fn simulate(mut nums: Vec<Vec<(bool, i32)>>) -> i32 {
+	fn safe_increment(nums: &mut Vec<Vec<(bool, i32)>>, x: i32, y: i32) {
+		if x < 0 || y < 0 || x > 9 || y > 9 {
+			return;
+		}
+		nums[x as usize][y as usize].1 += 1;
+	}
+
+	for i in 0.. {
+		for line in &mut nums {
+			for n in line {
+				if n.1 > 9 {
+					n.1 = 0
+				}
+				n.0 = false;
+				n.1 += 1;
+			}
+		}
+
+		let mut done = false;
+		while !done {
+			done = true;
+			for x in 0..10 {
+				for y in 0..10 {
+					let val = &mut nums[x as usize][y as usize];
+					if val.0 == false && val.1 > 9 {
+						val.0 = true;
+						safe_increment(&mut nums, x - 1, y - 1);
+						safe_increment(&mut nums, x - 1, y);
+						safe_increment(&mut nums, x - 1, y + 1);
+						safe_increment(&mut nums, x, y + 1);
+						safe_increment(&mut nums, x + 1, y + 1);
+						safe_increment(&mut nums, x + 1, y);
+						safe_increment(&mut nums, x + 1, y - 1);
+						safe_increment(&mut nums, x, y - 1);
+						done = false;
+					}
+				}
+			}
+		}
+
+		if nums.iter().all(|l| l.iter().all(|v| v.0)) {
+			return i + 1;
+		}
+	}
+	0
+}
+
+fn main() {
+	let contents = fs::read_to_string("input.txt").unwrap();
+
+	let nums: Vec<Vec<(bool, i32)>> = contents
+		.lines()
+		.map(parse_num_line)
+		.map(|l| l.iter().map(|n| (false, *n)).collect())
+		.collect();
+
+	println!("{:?}", simulate(nums));
+}
