6546cf029468014af07bd6923b7864b2a1d95db4
Eero Lehtinen <eero@eerolehtinen.fi>
2022-07-25 02:29:38 +0300

    17

---
+++ 17/src/part1.rs
@@ -0,0 +1,56 @@
+fn calc_lowest_x_velocity(target_start: i64, target_end: i64) -> i64 {
+	for i in 0..target_end {
+		let mut pos = 0;
+		let mut velocity = i;
+		loop {
+			pos += velocity;
+			if velocity > 0 {
+				velocity -= 1
+			}
+
+			if pos >= target_start && pos <= target_end {
+				return i;
+			}
+			if pos > target_end || velocity == 0 {
+				break;
+			}
+		}
+	}
+	panic!("unreachable");
+}
+
+fn calc_highest_y_velocity(target_start: i64, target_end: i64) -> (i64, i64) {
+	for i in (0..10_000).rev() {
+		let mut pos = 0;
+		let mut velocity = i;
+		let mut highest_y = 0;
+		loop {
+			pos += velocity;
+			velocity -= 1;
+
+			if pos > highest_y {
+				highest_y = pos;
+			}
+
+			if pos >= target_start && pos <= target_end {
+				return (i, highest_y);
+			}
+			if pos < target_start {
+				break;
+			}
+		}
+	}
+	panic!("unreachable");
+}
+
+fn main() {
+	let x_target = (150, 193);
+	let y_target = (-136, -86);
+
+	let x_vel = calc_lowest_x_velocity(x_target.0, x_target.1);
+	let (y_vel, highest_y) = calc_highest_y_velocity(y_target.0, y_target.1);
+
+	println!("x_vel: {x_vel:?}");
+	println!("y_vel: {y_vel:?}");
+	println!("highest_y: {highest_y:?}");
+}
---
+++ 17/src/part2.rs
@@ -0,0 +1,93 @@
+fn calc_x_velocities(target: (i64, i64)) -> Vec<i64> {
+	let mut res = Vec::new();
+	for i in 0..10_000 {
+		let mut pos = 0;
+		let mut velocity = i;
+		loop {
+			pos += velocity;
+			if velocity > 0 {
+				velocity -= 1
+			}
+
+			if pos >= target.0 && pos <= target.1 {
+				res.push(i);
+				break;
+			}
+			if pos > target.1 || velocity == 0 {
+				break;
+			}
+		}
+	}
+	res
+}
+
+fn calc_y_velocities(target: (i64, i64)) -> Vec<i64> {
+	let mut res = Vec::new();
+	for i in -10_000..10_000 {
+		let mut pos = 0;
+		let mut velocity = i;
+		let mut highest_y = 0;
+		loop {
+			pos += velocity;
+			velocity -= 1;
+
+			if pos > highest_y {
+				highest_y = pos;
+			}
+
+			if pos >= target.0 && pos <= target.1 {
+				res.push(i);
+				break;
+			}
+			if pos < target.0 {
+				break;
+			}
+		}
+	}
+	res
+}
+
+fn simulate(mut velocity: (i64, i64), target_x: (i64, i64), target_y: (i64, i64)) -> bool {
+	let mut pos = (0, 0);
+	loop {
+		pos.0 += velocity.0;
+		pos.1 += velocity.1;
+
+		if velocity.0 > 0 {
+			velocity.0 -= 1;
+		}
+
+		velocity.1 -= 1;
+
+		if pos.0 >= target_x.0 && pos.0 <= target_x.1 && pos.1 >= target_y.0 && pos.1 <= target_y.1
+		{
+			return true;
+		}
+		if pos.0 > target_x.1 || pos.1 < target_y.0 {
+			break;
+		}
+	}
+	false
+}
+
+fn main() {
+	let x_target = (150, 193);
+	let y_target = (-136, -86);
+
+	let xs = calc_x_velocities(x_target);
+	let ys = calc_y_velocities(y_target);
+
+	let mut velocities = Vec::new();
+	for x in xs.iter() {
+		for y in ys.iter() {
+			if simulate((*x, *y), x_target, y_target) {
+				velocities.push((*x, *y));
+			}
+		}
+	}
+
+	println!("xs: {xs:?}");
+	println!("ys: {ys:?}");
+	println!("vel: {:?}", velocities);
+	println!("count: {:?}", velocities.len());
+}
